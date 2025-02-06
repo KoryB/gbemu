ca10c3e2255750e0418b981777a4f01dc79464ca
KoryB <byrnekory@gmail.com>
2025-02-06 07:50:32 -0500

    Added ld op codes

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -26,5 +26,44 @@ namespace gb::cpu
             h(r_af.high & 0x0F == 0 && a_low_nibble != 0);
         }
     }
+
+    std::uint8_t &cpu::get_r8(const std::uint8_t r8_id)
+    {
+        switch (r8_id)
+        {
+            case 0x00:
+                return r_bc.high;
+            case 0x01:
+                return r_bc.low;
+            case 0x02:
+                return r_de.high;
+            case 0x03:
+                return r_de.low;
+            case 0x04:
+                return r_hl.high;
+            case 0x05:
+                return r_hl.low;
+            case 0x07:
+            default:
+                // TODO KB: What to do? Return A for now I guess
+                return r_af.high;
+        }
+    }
+
+    reg &cpu::get_r16(const std::uint8_t r16_id)
+    {
+        switch (r16_id)
+        {
+            case 0x000:
+            case 0x001:
+            default:
+                return r_bc;
+            case 0x010:
+            case 0x011:
+                return r_de;
+
+        }
+    }
+
 }
 
--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -15,19 +15,19 @@ namespace gb::mem
 
 namespace gb::cpu
 {
-    union reg
+    // TODO KB: This doesn't work because of endianness?
+    struct reg
     {
-        std::uint16_t full;
-
-        struct
-        {
-            std::uint8_t high;
-            std::uint8_t low;
-        };
+        std::uint8_t high;
+        std::uint8_t low;
 
         // ReSharper disable once CppNonExplicitConvertingConstructor
-        reg(const std::uint16_t full) : full(full) {} // NOLINT(*-explicit-constructor)
+        reg(const std::uint16_t full) // NOLINT(*-explicit-constructor)
+            : high((full & 0xFF00) >> 8)
+            , low(full & 0x00FF) { }
         reg(const std::uint8_t high, const std::uint8_t low) : high(high), low(low) {}
+
+        [[nodiscard]] std::uint16_t full() const { return (high << 8) | low; }
     };
 
     class cpu
@@ -68,7 +68,33 @@ namespace gb::cpu
         void c(const bool set) { r_af.low = (r_af.low & ~0x10) | (set << 1); }
 
     private:
+        std::uint8_t &get_r8(std::uint8_t r8_id);
+        reg &get_r16(std::uint8_t r16_id);
+
         std::uint8_t read_pc();
+
+        // These are all separate because they have different cycle properties
+        void ld_r8_r8(std::uint8_t r8_id_1, std::uint8_t r8_id_2);      // LD r8,r8
+        void ld_r8_n8(std::uint8_t r8_id);                              // LD r8,n8
+        void ld_r16_n16(std::uint8_t r16_id);                           // LD r16,n16
+        void ld_ihl_r8(std::uint8_t r8_id);                             // LD [HL],r8
+        void ld_ihl_n8();                                               // LD [HL],n8
+        void ld_r8_ihl(std::uint8_t r8_id);                             // LD r8,[HL]
+        void ld_ir16_a(std::uint8_t r16_id);                            // LD [r16],A
+        void ld_in16_a();                                               // LD [n16],A
+        void ldh_in16_a();                                              // LDH [n16],A
+        void ldh_ic_a();                                                // LDH [C],A
+        void ld_a_ir16(std::uint8_t reg16_id);                          // LD A,[r16]
+        void ld_a_in16();                                               // LD A,[n16]
+        void ldh_a_in16();                                              // LDH A,[n16]
+        void ldh_a_ic();                                                // LDH A,[C]
+        void ldi_ihl_a();                                               // LD [HLI],A
+        void ldd_ihl_a();                                               // LD [HLD],A
+        void ldi_a_ihl();                                               // LD A,[HLI]
+        void ldd_a_ihl();                                               // LD A,[HLD]
+
+
+
         std::shared_ptr<mem::memory_map> memory_map;
     };
 }
